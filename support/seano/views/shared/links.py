4bc05ce7e8eca50ebf91d3757d51d944a7f9d51c
Andrew Keller <andrew@kellerfarm.com>
2023-11-15 11:28:28 -0500

    fix: Allow GH tickets in change notes
    This commit exists for posterity only; data unrelated to the history of the QA Notes Seano formatter plugin has been removed.
    Original repository: zarf.git
    Original commit ID: 73b3d3b7adb8d9a4c0626465d0ea3b6e88843867

--- support/seano/views/shared/links.py
+++ support/seano/views/shared/links.py
@@ -5,7 +5,7 @@ Infrastructure to help process links to external servers
 """
 import re
 
-
+github_issue_url_regex = re.compile(r'^https?://github.com/[^/]+/([^/]+)/issues/([0-9]+)$')
 jira_url_regex = re.compile(r'^https?://[^/]*jira[^/]*/browse/([A-Z]+\-[0-9]+)$')
 redmine_url_regex = re.compile(r'^https?://[^/]*redmine[^/]*/issues/([0-9]+)$')
 
@@ -14,6 +14,9 @@ def get_ticket_display_name(url):
     '''
     Returns the display name of the ticket addressable at the given URL.
     '''
+    m = github_issue_url_regex.match(url)
+    if m: return '/'.join([m.group(1), m.group(2)])
+
     m = jira_url_regex.match(url)
     if m: return m.group(1)
 
